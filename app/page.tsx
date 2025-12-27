1824830d2f426fb204d7248975d844347c5324fb
Sam Adeyemi <sammyadeyemi@gmail.com>
2025-12-27 00:18:23 +0000

    fix: completely clean page.tsx - remove all duplicate exports

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,25 @@
-import Hero from './components/Hero'
-import Problem from './components/Problem'
-import Solution from './components/Solution'
-import HowItWorks from './components/HowItWorks'
-import Pricing from './components/Pricing'
-import FAQ from './components/FAQ'
-import CTA from './components/CTA'
-import Footer from './components/Footer'
-import FreeTools from './components/FreeTools'
+import Hero from './components/Hero';
+import Problem from './components/Problem';
+import Solution from './components/Solution';
+import FreeTools from './components/FreeTools';
+import HowItWorks from './components/HowItWorks';
+import Pricing from './components/Pricing';
+import FAQ from './components/FAQ';
+import CTA from './components/CTA';
+import Footer from './components/Footer';
 
-export default function Home() {
+export default function HomePage() {
   return (
     <main>
       <Hero />
       <Problem />
       <Solution />
+      <FreeTools />
       <HowItWorks />
-	  <FreeTools />
       <Pricing />
       <FAQ />
       <CTA />
       <Footer />
     </main>
-  )
+  );
 }
