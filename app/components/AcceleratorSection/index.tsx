c4b9c030453b1d19ab98be4da5196ec6c2bb9321
Sam Adeyemi <sammyadeyemi@gmail.com>
2025-12-28 09:59:20 +0000

    fix: flatten AcceleratorSection structure and remove index wrapper

--- app/components/AcceleratorSection/index.tsx
+++
@@ -1,8 +0,0 @@
-// components/AcceleratorSection/index.js
-'use client'; // This makes it a Client Component
-
-import AcceleratorSectionClient from './AcceleratorSectionClient';
-
-export default function AcceleratorSection() {
-  return <AcceleratorSectionClient />;
-}
